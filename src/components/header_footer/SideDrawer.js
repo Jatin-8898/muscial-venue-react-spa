404d307560b9b4e714c7c2cecc4720a4416cd948
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-22 23:11:22 +0530

    Added SideDrawer

---
+++ src/components/header_footer/SideDrawer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+
+const SideDrawer = (props) => {
+    return (
+        <Drawer
+            anchor="right"
+            open={props.open}
+            onClose={()=>props.onClose(false)}
+        >
+        Drawer test
+        </Drawer>
+    );
+};
+
+export default SideDrawer;
