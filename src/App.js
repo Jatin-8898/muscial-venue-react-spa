07ea1bc99abe8db1029f8f3872581d429ab623a7
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-22 23:56:12 +0530

    Imported the Featured Component & loaded

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './resources/styles.css';
 
 import Header from './components/header_footer/Header';
+import Featured from './components/featured/index';
 
 class App extends Component {
   render() {
     return (
       <div className="App" style={{ height:'1500px', background:'cornflowerblue'}}>
         <Header/>
+        <Featured/>
       </div>
     );
   }
