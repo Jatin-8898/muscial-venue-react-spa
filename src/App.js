266ecea643c87c7e6bac771c27618dd9971adaed
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-22 21:13:09 +0530

    Addded the required stuff to load Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import './resources/styles.css';
 
+import Header from './components/header_footer/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        HeLLO
+        <Header/>
       </div>
     );
   }
