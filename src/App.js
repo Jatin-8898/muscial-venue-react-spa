2ef2696014d71f5f5175f0c037d91095ab3bdb4b
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-22 23:29:26 +0530

    Added some height & color for scrolling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './components/header_footer/Header';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{ height:'1500px', background:'cornflowerblue'}}>
         <Header/>
       </div>
     );
