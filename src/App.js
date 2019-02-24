4f7e5ca01162b91355103cb2bac9233a9dbf761b
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-24 23:26:39 +0530

    Loaded the Highlights Component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import './resources/styles.css';
 
 import Header from './components/header_footer/Header';
-import Featured from './components/featured/index';
-import VenueInfo from './components/venue_info/index';
+import Featured from './components/featured';
+import VenueInfo from './components/venue_info';
+import Highlights from './components/highlights';
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height:'1500px', background:'cornflowerblue'}}>
+      <div className="App" style={{ height:'1500px', background:'white'}}>
         <Header/>
         <Featured/>
         <VenueInfo/>
+        <Highlights/>
       </div>
     );
   }
