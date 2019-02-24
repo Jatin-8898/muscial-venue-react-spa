13f4314c439b5b178ca2555b67b6d47c4100a509
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-24 23:25:09 +0530

    Created Highlights componenet

---
+++ src/components/highlights/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Description from './Description';
+
+const Highlights = () => {
+  return (
+    <div className="highlight_wrapper">
+      <Description />
+    </div>
+  )
+}
+export default Highlights;
