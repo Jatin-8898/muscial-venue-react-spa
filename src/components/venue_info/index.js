f80b071461b26327e3af61aa25138193c11e5d6a
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-24 21:56:15 +0530

    Added the Zoom animation in the Venu Info Component

--- src/components/venue_info/index.js
+++ src/components/venue_info/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Zoom from 'react-reveal/Zoom';
 
 import icon_calender from '../../resources/images/icons/calendar.png';
 import icon_location from '../../resources/images/icons/location.png';
@@ -9,48 +10,52 @@ import icon_location from '../../resources/images/icons/location.png';
       <div className="center_wrapper">
           <div className="vn_wrapper">
 
-            <div className="vn_item">
-              <div className="vn_outer">
-                <div className="vn_inner">
-					<div className="vn_icon_square  bck_red"></div>
-					<div 
-						className="vn_icon"
-						style={{
-							background: `url(${icon_calender})`
-						}}
-					>
-          			</div>
-					 <div className="vn_title">
-						Event Date & Time
-					 </div> 
-					 <div className="vn_desc">
-						7 August 2019 @ 8:00 pm
-					 </div> 
-                </div>
-              </div>
-            </div>
+            <Zoom duration={700}>
+				<div className="vn_item">
+					<div className="vn_outer">
+						<div className="vn_inner">
+							<div className="vn_icon_square  bck_red"></div>
+							<div 
+								className="vn_icon"
+								style={{
+									background: `url(${icon_calender})`
+								}}
+							>
+							</div>
+							<div className="vn_title">
+								Event Date & Time
+							</div> 
+							<div className="vn_desc">
+								7 August 2019 @ 8:00 pm
+							</div> 
+						</div>
+					</div>
+				</div>
+			</Zoom>
 
 
-			<div className="vn_item">
-              <div className="vn_outer">
-                <div className="vn_inner">
-					<div className="vn_icon_square  bck_yellow"></div>
-					<div 
-						className="vn_icon"
-						style={{
-							background: `url(${icon_location})`
-						}}
-					>
-          			</div>
-					 <div className="vn_title">
-						Event Location
-					 </div> 
-					 <div className="vn_desc">
-						2th Street, Orelands, New York
-					 </div> 
-                </div>
-              </div>
-            </div>
+			<Zoom delay={500} duration={700}>
+				<div className="vn_item">
+					<div className="vn_outer">
+						<div className="vn_inner">
+							<div className="vn_icon_square  bck_yellow"></div>
+							<div 
+								className="vn_icon"
+								style={{
+									background: `url(${icon_location})`
+								}}
+							>
+							</div>
+							<div className="vn_title">
+								Event Location
+							</div> 
+							<div className="vn_desc">
+								2th Street, Orelands, New York
+							</div> 
+						</div>
+					</div>
+				</div>
+			</Zoom>
 
           </div>
       </div>
