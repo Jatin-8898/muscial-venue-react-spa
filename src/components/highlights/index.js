5c24943fc3342dc50c9e613853ae7bda37838bb3
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-24 23:34:59 +0530

    Loaded the Discount Comp

--- src/components/highlights/index.js
+++ src/components/highlights/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Description from './Description';
+import Discount from './Discount';
 
 const Highlights = () => {
   return (
     <div className="highlight_wrapper">
       <Description />
+      <Discount />
     </div>
   )
 }
