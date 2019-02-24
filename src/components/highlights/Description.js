aaffa0db3abc949980e76b776184017f7cbf4ba9
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-24 23:25:31 +0530

    Created Description componenet

---
+++ src/components/highlights/Description.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Slide from 'react-reveal/Slide';
+
+const Description =  () => {
+  return (
+    <Slide left>
+        <div className="center_wrapper">
+            <h2>Highlights</h2>
+            <div className="highlight_description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+        </div>
+    </Slide>
+    
+  )
+}
+
+export default Description;
