ae348c58851da7e61491e3bb80557e2937a372ca
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-25 19:50:17 +0530

    Loaded the Pricing Comp

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/header_footer/Header';
 import Featured from './components/featured';
 import VenueInfo from './components/venue_info';
 import Highlights from './components/highlights';
+import Pricing from './components/pricing';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
         <Featured/>
         <VenueInfo/>
         <Highlights/>
+        <Pricing/>
       </div>
     );
   }
