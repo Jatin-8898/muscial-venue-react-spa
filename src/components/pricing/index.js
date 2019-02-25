079e104733608330de25c259fcc86289208d5ed6
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-25 19:35:25 +0530

    Created the Pricing Comp

---
+++ src/components/pricing/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Pricing extends Component {
+  render() {
+    return (
+      <div className="bck_black"> 
+        <div className="center_wrapper pricing_section"> 
+            <h2>Pricing</h2>
+        </div>
+
+        <div className="pricing_wrapper"> 
+            boxes
+        </div>
+
+      </div>
+    )
+  }
+}
+
+export default  Pricing;
