ae7abafd3514d106f8f8c724b693dc5f7a6a818a
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-25 20:36:32 +0530

    Made the Element for the Scroll to reference

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './resources/styles.css';
+import { Element } from 'react-scroll';
 
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';
@@ -14,12 +15,29 @@ class App extends Component {
     return (
       <div className="App" style={{ height:'1500px', background:'white'}}>
         <Header/>
-        <Featured/>
-        <VenueInfo/>
-        <Highlights/>
-        <Pricing/>
-        <Location/>
+
+        <Element name="featured">
+          <Featured/>
+        </Element>
+
+        <Element name="venueinfo">
+          <VenueInfo/>
+        </Element>
+        
+        <Element name="highlights">
+          <Highlights/>
+        </Element>
+        
+        <Element name="pricing">
+          <Pricing/>
+        </Element>
+
+        <Element name="location">
+          <Location/>
+        </Element>
+
         <Footer/>
+
       </div>
     );
   }
