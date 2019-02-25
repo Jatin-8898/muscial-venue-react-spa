c49f6074e4f1b4c55851fe13ff7923a4608b7495
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-25 20:36:02 +0530

    Added the Scroll in SideDrawer Comp

--- src/components/header_footer/SideDrawer.js
+++ src/components/header_footer/SideDrawer.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { scroller } from 'react-scroll';
+
+
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 
 const SideDrawer = (props) => {
+
+    const scrollToElement = (element) => {
+        scroller.scrollTo(element,{
+            duration: 1500,
+            delay:100,
+            smooth: true,
+            offset: -150
+        });
+        props.onClose(false)
+    }
+
     return (
         <Drawer
             anchor="right"
@@ -11,24 +25,24 @@ const SideDrawer = (props) => {
             onClose={()=>props.onClose(false)}
         >
         <List component="nav">
-            <ListItem button onClick={()=>console.log('TEST')}>
+            <ListItem button onClick={()=> scrollToElement('featured')}>
                 Event starts in
             </ListItem>
 
-            <ListItem button onClick={()=>console.log('Venue NFO ')}>
-                Venue NFO  
+            <ListItem button onClick={()=> scrollToElement('venueinfo')}>
+                Venue NFO
             </ListItem>
 
-            <ListItem button onClick={()=>console.log('Highlights')}>
+            <ListItem button onClick={()=> scrollToElement('highlights')}>
                 Highlights
             </ListItem>
 
-            <ListItem button onClick={()=>console.log('Pricing')}>
-                Pricing 
+            <ListItem button onClick={()=> scrollToElement('pricing')}>
+                Pricing
             </ListItem>
-            
-            <ListItem button onClick={()=>console.log('Location')}>
-                Location 
+
+            <ListItem button onClick={()=> scrollToElement('location')}>
+                Location
             </ListItem>
         </List>
         
